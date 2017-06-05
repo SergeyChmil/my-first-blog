0313a14339f22d3942d64cb1b88e705430fa35b6
tchmil <sergeytchmil@gmail.com>
2017-06-05 12:43:05 +0300

    modified templates to display posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+# from django.http import JsonResponse
+# from django.core import serializers
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# def post_list(request):
+#     posts = serializers.serialize('json', Post.objects.all())
+#     response = JsonResponse(posts, safe=False)
+#     return render(request, 'blog/post_list.html', {'posts': response})
